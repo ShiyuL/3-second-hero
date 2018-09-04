7581695cb1b4f7793bcd4c90d798da3d17e76b8a
Guiyang <hanguiyang@yahoo.com>
2018-09-04 17:56:59 -0400

    putting a image to test

--- main.lua
+++ main.lua
@@ -3,5 +3,9 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
-
--- Your code here
+local screen_adjustment = 0.3
+local background = display.newImage("\\Assets\\Statics\\Images\\open_background.jpg")
+background.xScale = (screen_adjustment  * background.contentWidth)/background.contentWidth
+background.yScale = background.xScale
+background.x = display.contentWidth / 2
+background.y = display.contentHeight / 2
