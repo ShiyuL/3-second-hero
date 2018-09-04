3809bb44de2dcaa13f7482abfe872b27a723b7bb
Guiyang <hanguiyang@yahoo.com>
2018-09-04 18:45:14 -0400

    restructure the folder

--- project/main.lua
+++ project/main.lua
@@ -3,9 +3,14 @@
 -- main.lua
 --
 -----------------------------------------------------------------------------------------
+
+halfW = display.contentWidth / 2
+halfH = display.contentHeight / 2
+
 local screen_adjustment = 0.3
-local background = display.newImage("\\Assets\\Statics\\Images\\open_background.jpg")
+local background = display.newImage("\\Assets\\Statics\\Images\\open_background.jpg", halfW, halfH)
 background.xScale = (screen_adjustment  * background.contentWidth)/background.contentWidth
 background.yScale = background.xScale
-background.x = display.contentWidth / 2
-background.y = display.contentHeight / 2
+
+local title_text = "3 Seconds Hero"
+local title = display.newText(title_text, halfW, halfH)
