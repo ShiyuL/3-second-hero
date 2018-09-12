aebffbad28fb097427cf844037742b2b0686ecd8
Han, Guiyang <Guiyang_Han@McAfee.com>
2018-09-12 16:30:16 -0400

    add in the hero

--- mainProject/game.lua
+++ mainProject/game.lua
@@ -6,42 +6,65 @@ physics.start()
 local composer = require( "composer" )
 local scene = composer.newScene()
 print("at game.lua")
-
+print(display.contentWidth)
+print(display.contentHeight)
 function scene:create(event)
 	local screenGroup = self.view
 
-	mine1 = display.newImage("\\Assets\\Statics\\Images\\mine.png")
-	mine1.xScale = 0.1
-	mine1.yScale = 0.1
-    mine1.x = 500
-    mine1.y = 100
-    mine1.speed = math.random(2,6)
-    mine1.initY = mine1.y
-    mine1.amp = math.random(20,100)
-    mine1.angle = math.random(1,360)
-    physics.addBody(mine1, "static", {density=.1, bounce=0.1, friction=.2, radius=12})
-	screenGroup:insert(mine1)
+	hero = display.newImage("\\Assets\\Statics\\Images\\hero.png")
+	hero.xScale = 0.1
+	hero.yScale = 0.1
+	hero.x = 160
+	hero.y = 460
+	screenGroup:insert(hero)
+
+	enemy1 = display.newImage("\\Assets\\Statics\\Images\\enemy.png")
+	enemy1.xScale = 0.1
+	enemy1.yScale = 0.1
+    enemy1.x = 100
+    enemy1.y = -50
+    enemy1.speed = math.random(2,6)
+    enemy1.initX = enemy1.x
+    enemy1.amp = math.random(20,100)
+    enemy1.angle = math.random(1,360)
+    physics.addBody(enemy1, "static", {density=.1, bounce=0.1, friction=.2, radius=12})
+	screenGroup:insert(enemy1)
 
 end
 
-function moveMines(self,event)
-	if self.x < -50 then
-		 self.x = 500
-         self.y = math.random(90,220)
+function touchScreen(event)
+    print("touch")
+   if event.phase == "began" then
+
+   end
+   
+   if event.phase == "ended" then
+
+   end
+
+end
+
+
+function moveEnemies(self,event)
+	if self.y > 500 then
+		 self.y = -50
+		 -- pad left and right for about 20 pixels
+         self.x = math.random(20,300)
          self.speed = math.random(2,6)
          self.amp = math.random(20,100)
          self.angle = math.random(1,360)
 	else 
-		self.x = self.x - self.speed
+		self.y = self.y + self.speed
 		self.angle = self.angle + .1
-		self.y = self.amp*math.sin(self.angle)+self.initY
 	end
 end
 
 
 function scene:show(event)
-	mine1.enterFrame = moveMines
-    Runtime:addEventListener("enterFrame", mine1)
+	Runtime:addEventListener("touch", touchScreen)
+	
+	enemy1.enterFrame = moveEnemies
+    Runtime:addEventListener("enterFrame", enemy1)
 end
 
 function scene:hide(event)
@@ -49,7 +72,8 @@ function scene:hide(event)
 end
 
 function scene:destroy(event)
-	Runtime:removeEventListener("enterFrame", mine1)
+	Runtime:removeEventListener("enterFrame", enemy1)
+	Runtime:removeEventListener("touch", touchScreen)
 end
 
 
